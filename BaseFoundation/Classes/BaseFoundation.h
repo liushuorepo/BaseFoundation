3ae5101976b9a3d9e863c5021dfe38a810b7fcf6
liushuo <liushuo@alaxiaoyou.com>
2017-06-30 15:31:03 +0800

    添加.h文件，方便调用

---
+++ BaseFoundation/Classes/BaseFoundation.h
@@ -0,0 +1,27 @@
+//
+//  BaseFoundation.h
+//  Pods
+//
+//  Created by Andy on 2017/6/30.
+//
+//
+
+#ifndef BaseFoundation_h
+#define BaseFoundation_h
+
+#import "UITextFieldLimit.h"
+#import "UITextViewPlaceHolder.h"
+#import "NSString+SHA.h"
+#import "NSString+Additions.h"
+#import "UIImage+reSize.h"
+#import "XYEdgeInsetsButton.h"
+#import "XYSearchBar.h"
+#import "Notification.h"
+#import "GCDTimer.h"
+#import "DefaultView.h"
+#import "BaseView.h"
+#import "BaseTableViewCell.h"
+#import "BaseCollectionViewCell.h"
+
+
+#endif /* BaseFoundation_h */
